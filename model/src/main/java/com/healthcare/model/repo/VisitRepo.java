342fe15643ec19d274b71d5b3ce49f9f16aaa517
ljurak <lucas.jurak@gmail.com>
2019-12-04 17:05:04 +0100

    changed query parameter type

--- model/src/main/java/com/healthcare/model/repo/VisitRepo.java
+++ model/src/main/java/com/healthcare/model/repo/VisitRepo.java
@@ -9,14 +9,13 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.healthcare.model.entities.Doctor;
 import com.healthcare.model.entities.Visit;
 
 @Repository
 public interface VisitRepo extends CrudRepository<Visit, Long> {
 	
-	@Query("select v from Visit v where v.doctor = :doctor and v.visitDate = :date and v.visitTime = :time")
-	Visit findVisitByDoctorAndDateTime(@Param("doctor") Doctor doctor, @Param("date") LocalDate date, @Param("time") LocalTime time);
+	@Query("select v from Visit v where v.doctor.id = :doctorId and v.visitDate = :date and v.visitTime = :time")
+	Visit findVisitByDoctorAndDateTime(@Param("doctorId") Long doctorId, @Param("date") LocalDate date, @Param("time") LocalTime time);
 	
 	@Query("select v from Visit v where v.patient.id = :patientId order by v.visitDate, v.visitTime")
 	List<Visit> findVisitsByPatient(@Param("patientId") Long patientId);
