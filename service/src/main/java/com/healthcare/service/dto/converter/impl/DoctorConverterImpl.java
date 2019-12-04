ac81d840116326273920f2db0fd7a70145e2fab5
ljurak <lucas.jurak@gmail.com>
2019-12-04 17:03:18 +0100

    added dto converters implementations

---
+++ service/src/main/java/com/healthcare/service/dto/converter/impl/DoctorConverterImpl.java
@@ -0,0 +1,35 @@
+package com.healthcare.service.dto.converter.impl;
+
+import org.springframework.stereotype.Component;
+
+import com.healthcare.model.entities.Doctor;
+import com.healthcare.service.dto.DoctorDTO;
+import com.healthcare.service.dto.converter.DoctorConverter;
+
+@Component
+public class DoctorConverterImpl implements DoctorConverter {
+	
+	@Override
+	public Doctor convertFromDTO(DoctorDTO dto) {
+		Doctor entity = new Doctor();
+		entity.setFirstName(dto.getFirstName());
+		entity.setLastName(dto.getLastName());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setAddress(dto.getAddress());
+		entity.setPhoneNumber(dto.getPhoneNumber());
+		entity.setEmail(dto.getEmail());
+		return entity;
+	}
+
+	@Override
+	public DoctorDTO convertFromEntity(Doctor entity) {
+		DoctorDTO dto = new DoctorDTO();
+		dto.setFirstName(entity.getFirstName());
+		dto.setLastName(entity.getLastName());
+		dto.setBirthDate(entity.getBirthDate());
+		dto.setAddress(entity.getAddress());
+		dto.setPhoneNumber(entity.getPhoneNumber());
+		dto.setEmail(entity.getEmail());
+		return dto;
+	}	
+}
---
+++ service/src/main/java/com/healthcare/service/dto/converter/impl/VisitConverterImpl.java
@@ -0,0 +1,54 @@
+package com.healthcare.service.dto.converter.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.healthcare.model.entities.Doctor;
+import com.healthcare.model.entities.Patient;
+import com.healthcare.model.entities.Visit;
+import com.healthcare.model.repo.DoctorRepo;
+import com.healthcare.model.repo.PatientRepo;
+import com.healthcare.service.dto.VisitDTO;
+import com.healthcare.service.dto.converter.VisitConverter;
+import com.healthcare.service.exception.DoctorException;
+import com.healthcare.service.exception.PatientException;
+
+@Component
+public class VisitConverterImpl implements VisitConverter {
+	
+	private PatientRepo patientRepo;
+	
+	private DoctorRepo doctorRepo;
+	
+	@Autowired
+	public VisitConverterImpl(PatientRepo patientRepo, DoctorRepo doctorRepo) {
+		this.patientRepo = patientRepo;
+		this.doctorRepo = doctorRepo;
+	}
+
+	@Override
+	public Visit convertFromDTO(VisitDTO dto) {
+		Visit entity = new Visit();
+		Patient patient = patientRepo.findById(dto.getPatientId())
+				.orElseThrow(() -> new PatientException("Patient with id: " + dto.getPatientId() + " does not exist"));
+		Doctor doctor = doctorRepo.findById(dto.getDoctorId())
+				.orElseThrow(() -> new DoctorException("Doctor with id: " + dto.getDoctorId() + " does not exist"));
+		entity.setPatient(patient);
+		entity.setDoctor(doctor);
+		entity.setVisitDate(dto.getVisitDate());
+		entity.setVisitTime(dto.getVisitTime());
+		entity.setStatus(dto.getVisitStatus());
+		return entity;
+	}
+
+	@Override
+	public VisitDTO convertFromEntity(Visit entity) {
+		VisitDTO dto = new VisitDTO();
+		dto.setPatientId(entity.getPatient().getId());
+		dto.setDoctorId(entity.getDoctor().getId());
+		dto.setVisitDate(entity.getVisitDate());
+		dto.setVisitTime(entity.getVisitTime());
+		dto.setVisitStatus(entity.getStatus());
+		return dto;
+	}	
+}
