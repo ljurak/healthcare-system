9eecbd80b9f0c0029346e22dfacffa708bf5373f
ljurak <lucas.jurak@gmail.com>
2019-12-08 02:52:15 +0100

    removed comment

--- model/src/main/java/com/healthcare/model/repo/JpaRepoPackage.java
+++ model/src/main/java/com/healthcare/model/repo/JpaRepoPackage.java
@@ -3,8 +3,6 @@ package com.healthcare.model.repo;
 /**
  * No-op marker interface for usage only in <code>@EnableJpaRepositories</code>
  * basePackageClasses attribute.
- * 
- * @author ljurak
  */
 public interface JpaRepoPackage {
 
