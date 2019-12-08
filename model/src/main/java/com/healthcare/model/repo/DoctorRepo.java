666a43733265ec63603268aa518fa37c5e70f40a
ljurak <lucas.jurak@gmail.com>
2019-12-08 02:51:16 +0100

    added some queries

--- model/src/main/java/com/healthcare/model/repo/DoctorRepo.java
+++ model/src/main/java/com/healthcare/model/repo/DoctorRepo.java
@@ -1,11 +1,22 @@
 package com.healthcare.model.repo;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.healthcare.model.entities.Doctor;
 
 @Repository
 public interface DoctorRepo extends CrudRepository<Doctor, Long> {
-
+	
+	@Query("select d from Doctor d order by d.lastName")
+	List<Doctor> findAllOrderByLastName();
+	
+	List<Doctor> findByLastNameContainingIgnoreCaseOrderByLastName(String lastName);
+	
+	@Query("select d from Doctor d where d.specialty.name = :specialty")
+	List<Doctor> findBySpecialty(@Param("specialty") String specialty);
 }
