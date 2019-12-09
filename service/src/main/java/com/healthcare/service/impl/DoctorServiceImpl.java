92454ce9c5fc0a72d523f58737770a73c576dc1d
ljurak <lucas.jurak@gmail.com>
2019-12-09 19:08:48 +0100

    implemented new methods

--- service/src/main/java/com/healthcare/service/impl/DoctorServiceImpl.java
+++ service/src/main/java/com/healthcare/service/impl/DoctorServiceImpl.java
@@ -1,5 +1,7 @@
 package com.healthcare.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -7,6 +9,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.healthcare.model.entities.Doctor;
 import com.healthcare.model.repo.DoctorRepo;
 import com.healthcare.service.DoctorService;
+import com.healthcare.service.dto.DoctorDTO;
+import com.healthcare.service.dto.converter.DoctorConverter;
 import com.healthcare.service.exception.DoctorException;
 
 @Service
@@ -14,15 +18,30 @@ import com.healthcare.service.exception.DoctorException;
 public class DoctorServiceImpl implements DoctorService {
 
 	private DoctorRepo doctorRepo;
+	private DoctorConverter doctorConverter;
 	
 	@Autowired
-	public DoctorServiceImpl(DoctorRepo doctorRepo) {
+	public DoctorServiceImpl(DoctorRepo doctorRepo, DoctorConverter doctorConverter) {
 		this.doctorRepo = doctorRepo;
+		this.doctorConverter = doctorConverter;
 	}
 	
 	@Override
-	public Doctor getDoctorById(Long id) {
-		return doctorRepo.findById(id)
+	public List<DoctorDTO> getDoctors() {
+		List<Doctor> doctors = doctorRepo.findAllOrderByLastName();
+		return doctorConverter.convertFromEntity(doctors);
+	}
+	
+	@Override
+	public DoctorDTO getDoctorById(Long id) {
+		Doctor doctor = doctorRepo.findById(id)
 				.orElseThrow(() -> new DoctorException("Doctor with id: " + id + " does not exist"));
+		return doctorConverter.convertFromEntity(doctor);
+	}	
+
+	@Override
+	public List<DoctorDTO> getDoctorsByLastName(String lastName) {
+		List<Doctor> doctors = doctorRepo.findByLastNameContainingIgnoreCaseOrderByLastName(lastName);
+		return doctorConverter.convertFromEntity(doctors);
 	}	
 }
