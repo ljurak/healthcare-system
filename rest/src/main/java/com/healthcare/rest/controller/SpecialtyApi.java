3bd76957484c5f9e64186d4076cae8f4f9b60fac
ljurak <lucas.jurak@gmail.com>
2019-12-09 19:19:57 +0100

    added api for doctor specialties

---
+++ rest/src/main/java/com/healthcare/rest/controller/SpecialtyApi.java
@@ -0,0 +1,26 @@
+package com.healthcare.rest.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.healthcare.service.SpecialtyService;
+import com.healthcare.service.dto.SpecialtyDTO;
+
+@RestController
+@RequestMapping("/specialties")
+public class SpecialtyApi {
+	
+	private SpecialtyService specialtyService;
+
+	public SpecialtyApi(SpecialtyService specialtyService) {
+		this.specialtyService = specialtyService;
+	}
+	
+	@GetMapping
+	public List<SpecialtyDTO> getSpecialties() {
+		return specialtyService.getSpecialties();
+	}
+}
