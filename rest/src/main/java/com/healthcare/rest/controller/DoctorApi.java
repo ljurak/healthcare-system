ea808dcaeaee1c1e8d57b7165df8ea5a9a61631e
ljurak <lucas.jurak@gmail.com>
2019-12-09 19:11:59 +0100

    added new endpoints

--- rest/src/main/java/com/healthcare/rest/controller/DoctorApi.java
+++ rest/src/main/java/com/healthcare/rest/controller/DoctorApi.java
@@ -1,26 +1,40 @@
 package com.healthcare.rest.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.healthcare.model.entities.Specialty;
-import com.healthcare.model.repo.SpecialtyRepo;
+import com.healthcare.service.DoctorService;
+import com.healthcare.service.dto.DoctorDTO;
 
 @RestController
 @RequestMapping("/doctors")
 public class DoctorApi {
 	
-	private SpecialtyRepo specialtyRepo;
+	private DoctorService doctorService;
 
 	@Autowired
-	public DoctorApi(SpecialtyRepo specialtyRepo) {
-		this.specialtyRepo = specialtyRepo;
+	public DoctorApi(DoctorService doctorService) {
+		this.doctorService = doctorService;
 	}
 	
-	@GetMapping("/specialties")
-	public Iterable<Specialty> getSpecialties() {
-		return specialtyRepo.findAll();
+	@GetMapping
+	public List<DoctorDTO> getDoctors() {
+		return doctorService.getDoctors();
+	}
+	
+	@GetMapping("/{id}")
+	public DoctorDTO getDoctor(@PathVariable Long id) {
+		return doctorService.getDoctorById(id);
+	}
+	
+	@GetMapping(params = "lastname")
+	public List<DoctorDTO> getDoctorsByLastName(@RequestParam String lastname) {
+		return doctorService.getDoctorsByLastName(lastname);
 	}
 }
