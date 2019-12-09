129b92fd52c88a4bf83821fcf2c4341af0c40995
ljurak <lucas.jurak@gmail.com>
2019-12-09 18:19:28 +0100

    added new methods

--- service/src/main/java/com/healthcare/service/DoctorService.java
+++ service/src/main/java/com/healthcare/service/DoctorService.java
@@ -1,14 +1,35 @@
 package com.healthcare.service;
 
+import java.util.List;
+
 import com.healthcare.model.entities.Doctor;
+import com.healthcare.service.dto.DoctorDTO;
 
 public interface DoctorService {
 	
 	/**
+	 * Retrieves list of all doctors.
+	 * 
+	 * @return list of all patients
+	 */
+	List<DoctorDTO> getDoctors();
+	
+	/**
 	 * Retrieves doctor by given id.
+	 * Throws <code>DoctorException</code> if doctor
+	 * with given id does not exist.
 	 * 
 	 * @param id id
 	 * @return doctor 
+	 * @throws DoctorException
 	 */
 	Doctor getDoctorById(Long id);
+	
+	/**
+	 * Retrieves doctors with matching last name or its part.
+	 * 
+	 * @param lastName doctor last name or its part
+	 * @return list of doctors
+	 */
+	List<DoctorDTO> getDoctorsByLastName(String lastName);
 }
