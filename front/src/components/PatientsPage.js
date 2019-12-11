ef05f3866f5d5e56b84a41cffa4a8dc5faf39a85
ljurak <lucas.jurak@gmail.com>
2019-12-11 03:22:47 +0100

    extended functionality with PatientsList

--- front/src/components/PatientsPage.js
+++ front/src/components/PatientsPage.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import PatientSearchForm from './PatientSearchForm';
+import PatientsList from './PatientsList';
+import { getVisiblePatients } from '../reducers';
 
 class PatientsPage extends React.Component {
 	render() {
+		const { patients } = this.props;
 		return (
-			<PatientSearchForm />
+			<React.Fragment>
+				<PatientSearchForm />
+				<PatientsList patients={patients} />
+			</React.Fragment>
 		);
 	}
 }
 
-export default PatientsPage;
+const mapStateToProps = (state) => ({
+	patients: getVisiblePatients(state)
+});
+
+export default connect(mapStateToProps)(PatientsPage);
