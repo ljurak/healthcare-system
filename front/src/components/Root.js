97a1d733389cbcd29e1334e466a0b37379ecc8c2
ljurak <lucas.jurak@gmail.com>
2019-12-11 03:20:15 +0100

    added Root component

---
+++ front/src/components/Root.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import App from './App';
+
+const Root = ({store}) => (
+	<Provider store={store}>
+		<Router>
+			<App />
+		</Router>
+	</Provider>
+);
+
+export default Root;
--- front/src/index.js
+++ front/src/index.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
 
+import Root from './components/Root';
 import configureStore from './store/configureStore';
-import * as actions from './actions';
 
 const store = configureStore();
 
 ReactDOM.render(
-	<Provider store={store}>
-		<Router>
-			<App />
-		</Router>
-	</Provider>, 
+	<Root store={store} />, 
 	document.getElementById('root')
 );
