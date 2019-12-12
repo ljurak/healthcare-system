cf199d67624e8567d899bdc709cf6ca627005b4b
ljurak <lucas.jurak@gmail.com>
2019-12-13 00:57:30 +0100

    added header for list

--- front/src/components/PatientsList.js
+++ front/src/components/PatientsList.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 import PatientsListItem from './PatientsListItem';
 
-const PatientsList = ({ patients }) => {
+const PatientsList = ({ patients, header }) => {
 	return (
 		<React.Fragment>
-			<h3 className="patients-list-title">Search results</h3>
+			<h3 className="patients-list-title">{header}</h3>
 			<table className="patients-list">
 				<thead className="patients-list-header">
 					<tr>
