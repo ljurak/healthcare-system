3563adc621b7a89a546424e97bea6830a895a3c6
ljurak <lucas.jurak@gmail.com>
2019-12-15 22:46:39 +0100

    added new endpoints

--- rest/src/main/java/com/healthcare/rest/controller/DoctorApi.java
+++ rest/src/main/java/com/healthcare/rest/controller/DoctorApi.java
@@ -2,14 +2,24 @@ package com.healthcare.rest.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.healthcare.rest.exception.InvalidRequestException;
 import com.healthcare.service.DoctorService;
 import com.healthcare.service.dto.DoctorDTO;
 
@@ -30,6 +40,17 @@ public class DoctorApi {
 		return doctorService.getDoctors();
 	}
 	
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.CREATED)
+	public DoctorDTO processAddingNewDoctor(@RequestBody @Valid DoctorDTO doctorDTO, BindingResult result) {
+		if (result.hasErrors()) {
+			throw new InvalidRequestException("Invalid data format", result);
+		}
+		
+		DoctorDTO addedDoctor = doctorService.addDoctor(doctorDTO);
+		return addedDoctor;
+	}
+	
 	@GetMapping("/{id}")
 	public DoctorDTO getDoctor(@PathVariable Long id) {
 		return doctorService.getDoctorById(id);
@@ -38,5 +59,15 @@ public class DoctorApi {
 	@GetMapping(params = "lastname")
 	public List<DoctorDTO> getDoctorsByLastName(@RequestParam String lastname) {
 		return doctorService.getDoctorsByLastName(lastname);
+	}
+	
+	@PutMapping(path = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public DoctorDTO updateDoctor(@RequestBody @Valid DoctorDTO doctorDTO, BindingResult result, @PathVariable long id) {
+		if (result.hasErrors()) {
+			throw new InvalidRequestException("Invalid data format", result);
+		}
+		
+		DoctorDTO updatedDoctor = doctorService.updateDoctor(doctorDTO, id);
+		return updatedDoctor;
 	}
 }
