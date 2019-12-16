6bd86cf2c35bf683cd7d27b1ea0874d9ce6fd5cf
ljurak <lucas.jurak@gmail.com>
2019-12-16 03:06:25 +0100

    added crossorigin

--- rest/src/main/java/com/healthcare/rest/controller/SpecialtyApi.java
+++ rest/src/main/java/com/healthcare/rest/controller/SpecialtyApi.java
@@ -2,6 +2,7 @@ package com.healthcare.rest.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.healthcare.service.dto.SpecialtyDTO;
 
 @RestController
 @RequestMapping("/specialties")
+@CrossOrigin
 public class SpecialtyApi {
 	
 	private SpecialtyService specialtyService;
