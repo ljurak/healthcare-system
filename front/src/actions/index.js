32817dd3d1765c1bba7a2c7f4d44c24fbc062c9a
ljurak <lucas.jurak@gmail.com>
2019-12-17 23:51:03 +0100

    added actions for updating patients and doctors

--- front/src/actions/index.js
+++ front/src/actions/index.js
@@ -11,18 +11,21 @@ const makeRequestActionCreator = (type) => () => ({	type });
 const fetchPatientsRequest = makeRequestActionCreator(actions.FETCH_PATIENTS_REQUEST);
 const fetchDoctorsRequest = makeRequestActionCreator(actions.FETCH_DOCTORS_REQUEST);
 const addPatientRequest = makeRequestActionCreator(actions.ADD_PATIENT_REQUEST);
+const updatePatientRequest = makeRequestActionCreator(actions.UPDATE_PATIENT_REQUEST);
 
 const makeSuccessActionCreator = (type) => (payload) => ({ type, payload });
 
 const fetchPatientsSuccess = makeSuccessActionCreator(actions.FETCH_PATIENTS_SUCCESS);
 const fetchDoctorsSuccess = makeSuccessActionCreator(actions.FETCH_DOCTORS_SUCCESS);
 const addPatientSuccess = makeSuccessActionCreator(actions.ADD_PATIENT_SUCCESS);
+const updatePatientSuccess = makeSuccessActionCreator(actions.UPDATE_PATIENT_SUCCESS);
 
 const makeFailureActionCreator = (type) => (payload) => ({ type, payload, error: true});
 
 const fetchPatientsFailure = makeFailureActionCreator(actions.FETCH_PATIENTS_FAILURE);
 const fetchDoctorsFailure = makeFailureActionCreator(actions.FETCH_DOCTORS_FAILURE);
 const addPatientFailure = makeFailureActionCreator(actions.ADD_PATIENT_FAILURE);
+const updatePatientFailure = makeFailureActionCreator(actions.UPDATE_PATIENT_FAILURE);
 
 // ASYNC ACTION CREATORS
 
@@ -100,6 +103,17 @@ export const addPatient = (patient) => (dispatch) => {
 		.then(
 			response => dispatch(addPatientSuccess(normalize([ response ], schema.patientsListSchema))),
 			error => dispatch(addPatientFailure(error))
+		);
+};
+
+export const updatePatient = (patient, id) => (dispatch) => {
+	dispatch(updatePatientRequest());
+
+	return PatientsApi.updatePatient(patient, id)
+		.then(handleApiResponse)
+		.then(
+			response => dispatch(updatePatientSuccess(normalize([ response ], schema.patientsListSchema))),
+			error => dispatch(updatePatientFailure(error))
 		);
 };
 
