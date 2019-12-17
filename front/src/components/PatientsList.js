e6f237336fc4aa2f3d912f81545e5abae7b1f2ba
ljurak <lucas.jurak@gmail.com>
2019-12-17 23:52:25 +0100

    removed header

--- front/src/components/PatientsList.js
+++ front/src/components/PatientsList.js
@@ -2,24 +2,21 @@ import React from 'react';
 
 import PatientsListItem from './PatientsListItem';
 
-const PatientsList = ({ patients, header }) => {
+const PatientsList = ({ patients }) => {
 	return (
-		<React.Fragment>
-			<h3 className="patients-list-title">{header}</h3>
-			<table className="patients-list">
-				<thead className="patients-list-header">
-					<tr>
-						<th>Full name</th>
-						<th>Birth date</th>
-						<th>Address</th>
-						<th>Phone number</th>
-					</tr>
-				</thead>
-				<tbody>
-					{patients.map(patient => <PatientsListItem key={patient.id} patient={patient} />)}
-				</tbody>
-			</table>
-		</React.Fragment>
+		<table className="patients-list">
+			<thead className="patients-list-header">
+				<tr>
+					<th>Full name</th>
+					<th>Birth date</th>
+					<th>Address</th>
+					<th>Phone number</th>
+				</tr>
+			</thead>
+			<tbody>
+				{patients.map(patient => <PatientsListItem key={patient.id} patient={patient} />)}
+			</tbody>
+		</table>
 	);
 };
 
