95cc79d9e61e6757492a700ccc48634a1b0867ee
ljurak <lucas.jurak@gmail.com>
2019-12-18 02:08:41 +0100

    removed unused import

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { connect } from 'react-redux';
 
 import Header from './Header';
 import Footer from './Footer';
