163c6f17b1669da036c9d66facaf6e4a2e95e108
ljurak <lucas.jurak@gmail.com>
2019-12-21 05:50:48 +0100

    added protected routes

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Header from './Header';
 import Footer from './Footer';
+import MainContent from './MainContent';
 import PatientComponent from './patients/PatientComponent';
 import DoctorsPage from './DoctorsPage';
 import LoginPage from './LoginPage';
+import PrivateRoute from './PrivateRoute';
 
 const App = () => (
     <React.Fragment>
@@ -15,9 +17,11 @@ const App = () => (
         		<aside className="sidebar"></aside>
         		<main className="main-content">
         			<Switch>
-                        <Route path="/info" component={LoginPage} />
-        				<Route path="/patients" component={PatientComponent} />
-        				<Route path="/doctors" component={DoctorsPage} />
+                        <PrivateRoute exact path="/" component={MainContent} />
+                        <PrivateRoute path="/patients" component={PatientComponent} />
+                        <PrivateRoute path="/doctors" component={DoctorsPage} />
+                        <Route path="/login" component={LoginPage} />
+                        <Redirect from="*" to="/" />
                     </Switch>
         		</main>	                	
             </div>
