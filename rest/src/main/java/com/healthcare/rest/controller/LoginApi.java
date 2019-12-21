fa1df9e404aff0f8ff46ceacf1b215b8421fa699
ljurak <lucas.jurak@gmail.com>
2019-12-21 05:53:04 +0100

    added crossorigin

--- rest/src/main/java/com/healthcare/rest/controller/LoginApi.java
+++ rest/src/main/java/com/healthcare/rest/controller/LoginApi.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import com.healthcare.rest.security.LoginDTO;
 
 @RestController
 @RequestMapping("/login")
+@CrossOrigin
 public class LoginApi {
 	
 	private AuthenticationManager authenticationManager;
