1d27d0fd03aa6d6fc7a342ebc5c169cf03e7e776
ljurak <lucas.jurak@gmail.com>
2019-12-21 05:47:35 +0100

    added authorization header creator

--- front/src/api/index.js
+++ front/src/api/index.js
@@ -1,23 +1,37 @@
 const BASE_API_URL = 'http://localhost:8080';
 
+const createAuthorizationHeader = () => {
+	const token = localStorage.getItem('token');
+	if (token) {
+		return { 'Authorization': `Bearer ${token}` };
+	}
+	return {};
+};
+
 const PatientsApi = {};
 
 PatientsApi.fetchPatients = function() {
-	return fetch(`${BASE_API_URL}/patients`);
+	return fetch(`${BASE_API_URL}/patients`, {
+		headers: createAuthorizationHeader()
+	});
 };
 
 PatientsApi.fetchPatientsByLastname = function(lastname) {
-	return fetch(`${BASE_API_URL}/patients?lastname=${lastname}`);
+	return fetch(`${BASE_API_URL}/patients?lastname=${lastname}`, {
+		headers: createAuthorizationHeader()
+	});
 };
 
 PatientsApi.fetchPatientById = function(id) {
-	return fetch(`${BASE_API_URL}/patients/${id}`);
+	return fetch(`${BASE_API_URL}/patients/${id}`, {
+		headers: createAuthorizationHeader()
+	});
 };
 
 PatientsApi.addPatient = function(patient) {
 	return fetch(`${BASE_API_URL}/patients`, {
 		method: 'POST',
-		headers: { 'Content-Type': 'application/json' },
+		headers: { 'Content-Type': 'application/json', ...createAuthorizationHeader() },
 		body: JSON.stringify(patient)
 	});
 };
@@ -25,7 +39,7 @@ PatientsApi.addPatient = function(patient) {
 PatientsApi.updatePatient = function(patient, id) {
 	return fetch(`${BASE_API_URL}/patients/${id}`, {
 		method: 'PUT',
-		headers: { 'Content-Type': 'application/json' },
+		headers: { 'Content-Type': 'application/json', ...createAuthorizationHeader() },
 		body: JSON.stringify(patient)
 	});
 };
@@ -33,15 +47,31 @@ PatientsApi.updatePatient = function(patient, id) {
 const DoctorsApi = {};
 
 DoctorsApi.fetchDoctors = function() {
-	return fetch(`${BASE_API_URL}/doctors`);
+	return fetch(`${BASE_API_URL}/doctors`, {
+		headers: createAuthorizationHeader()
+	});
 };
 
 DoctorsApi.fetchDoctorsByLastname = function(lastname) {
-	return fetch(`${BASE_API_URL}/doctors?lastname=${lastname}`);
+	return fetch(`${BASE_API_URL}/doctors?lastname=${lastname}`, {
+		headers: createAuthorizationHeader()
+	});
 };
 
 DoctorsApi.fetchDoctorById = function(id) {
-	return fetch(`${BASE_API_URL}/doctors/${id}`);
+	return fetch(`${BASE_API_URL}/doctors/${id}`, {
+		headers: createAuthorizationHeader()
+	});
 };
 
-export { PatientsApi, DoctorsApi };
+const LoginApi = {};
+
+LoginApi.login = function(user) {
+	return fetch(`${BASE_API_URL}/login`, {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify(user)
+	});
+}
+
+export { PatientsApi, DoctorsApi, LoginApi };
