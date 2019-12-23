723a41e9551852969cc9bfd94eb0eb015799d475
ljurak <lucas.jurak@gmail.com>
2019-12-23 16:30:02 +0100

    added date and time picker for visit form

---
+++ front/src/components/DatePickerField.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+
+const DatePickerField = ({ id, name, value, onChange, onBlur }) => {
+
+	const handleChange = (value) => {
+		onChange(name, value);
+	};
+
+	const handleBlur = (e) => {
+		onBlur(e);
+	};
+
+	const minDate = new Date();
+	const maxDate = new Date();
+	maxDate.setMonth(maxDate.getMonth() + 6);
+
+	return (
+		<DatePicker
+			autocomplete="off"
+			id={id}
+			name={name}
+			selected={value}
+			onChange={handleChange}
+			onBlur={handleBlur}
+			minDate={minDate}
+			maxDate={maxDate}
+			dateFormat="yyyy-MM-dd"
+			placeholderText="YYYY-MM-DD" />
+	);	
+};
+
+export default DatePickerField;
---
+++ front/src/components/TimePickerField.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+
+const TimePickerField = ({ id, name, value, onChange, onBlur }) => {
+
+	const handleChange = (value) => {
+		onChange(name, value);
+	};
+
+	const handleBlur = (e) => {
+		onBlur(e);
+	};
+
+	const minTime = new Date();
+	minTime.setHours(8);
+	minTime.setMinutes(0);
+	const maxTime = new Date();
+	maxTime.setHours(15);
+	maxTime.setMinutes(0);
+	
+
+	return (
+		<DatePicker
+			autocomplete="off"
+			id={id}
+			name={name}
+			selected={value}
+			onChange={handleChange}
+			onBlur={handleBlur}
+			showTimeSelect
+			showTimeSelectOnly
+			timeIntervals={60}
+			minTime={minTime}
+			maxTime={maxTime}
+			dateFormat="HH:mm"
+			timeFormat="HH:mm"
+			placeholderText="HH:MM" />
+	);	
+};
+
+export default TimePickerField;
