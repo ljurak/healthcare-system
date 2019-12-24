110f3a70a56b2cf0ac20315855c07d4c3583b802
ljurak <lucas.jurak@gmail.com>
2019-12-24 03:28:30 +0100

    added new handler for not readable json

--- rest/src/main/java/com/healthcare/rest/controller/ApiExceptionHandler.java
+++ rest/src/main/java/com/healthcare/rest/controller/ApiExceptionHandler.java
@@ -3,12 +3,15 @@ package com.healthcare.rest.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.healthcare.rest.exception.ApiError;
@@ -44,6 +47,14 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 		
 		ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getMessage());
 		apiError.setFieldErrors(fieldValidationErrors);
+		return new ResponseEntity<>(apiError, apiError.getStatus());
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(
+			HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, "Malformed JSON request");
 		return new ResponseEntity<>(apiError, apiError.getStatus());
 	}
 }
