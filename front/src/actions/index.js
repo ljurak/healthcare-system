391f94d5b1875e51e49d5d96f14579956f0e0575
ljurak <lucas.jurak@gmail.com>
2019-12-28 13:08:02 +0100

    added actions for visits

--- front/src/actions/index.js
+++ front/src/actions/index.js
@@ -14,6 +14,7 @@ const fetchPatientRequest = makeRequestActionCreator(actions.FETCH_PATIENT_REQUE
 const fetchDoctorsRequest = makeRequestActionCreator(actions.FETCH_DOCTORS_REQUEST);
 const fetchDoctorRequest = makeRequestActionCreator(actions.FETCH_DOCTOR_REQUEST);
 const fetchSpecialtiesRequest = makeRequestActionCreator(actions.FETCH_SPECIALTIES_REQUEST);
+const fetchVisitsRequest = makeRequestActionCreator(actions.FETCH_VISITS_REQUEST);
 const addPatientRequest = makeRequestActionCreator(actions.ADD_PATIENT_REQUEST);
 const updatePatientRequest = makeRequestActionCreator(actions.UPDATE_PATIENT_REQUEST);
 const addDoctorRequest = makeRequestActionCreator(actions.ADD_DOCTOR_REQUEST);
@@ -28,6 +29,7 @@ const fetchPatientSuccess = makeSuccessActionCreator(actions.FETCH_PATIENT_SUCCE
 const fetchDoctorsSuccess = makeSuccessActionCreator(actions.FETCH_DOCTORS_SUCCESS);
 const fetchDoctorSuccess = makeSuccessActionCreator(actions.FETCH_DOCTOR_SUCCESS);
 const fetchSpecialtiesSuccess = makeSuccessActionCreator(actions.FETCH_SPECIALTIES_SUCCESS);
+const fetchVisitsSuccess = makeSuccessActionCreator(actions.FETCH_VISITS_SUCCESS);
 const addPatientSuccess = makeSuccessActionCreator(actions.ADD_PATIENT_SUCCESS);
 const updatePatientSuccess = makeSuccessActionCreator(actions.UPDATE_PATIENT_SUCCESS);
 const addDoctorSuccess = makeSuccessActionCreator(actions.ADD_DOCTOR_SUCCESS);
@@ -42,6 +44,7 @@ const fetchPatientFailure = makeFailureActionCreator(actions.FETCH_PATIENT_FAILU
 const fetchDoctorsFailure = makeFailureActionCreator(actions.FETCH_DOCTORS_FAILURE);
 const fetchDoctorFailure = makeFailureActionCreator(actions.FETCH_DOCTOR_FAILURE);
 const fetchSpecialtiesFailure = makeFailureActionCreator(actions.FETCH_SPECIALTIES_FAILURE);
+const fetchVisitsFailure = makeFailureActionCreator(actions.FETCH_VISITS_FAILURE);
 const addPatientFailure = makeFailureActionCreator(actions.ADD_PATIENT_FAILURE);
 const updatePatientFailure = makeFailureActionCreator(actions.UPDATE_PATIENT_FAILURE);
 const addDoctorFailure = makeFailureActionCreator(actions.ADD_DOCTOR_FAILURE);
@@ -117,17 +120,6 @@ export const updatePatient = (patient, id) => (dispatch) => {
 		.then(
 			response => dispatch(updatePatientSuccess(normalize(response, schema.patientSchema))),
 			error => dispatch(updatePatientFailure(error))
-		);
-};
-
-export const addVisit = (visit, patientId) => (dispatch) => {
-	dispatch(addVisitRequest());
-
-	return VisitsApi.addVisit(visit, patientId)
-		.then(handleApiResponse)
-		.then(
-			response => dispatch(addVisitSuccess(normalize([ response ], schema.visitsListSchema))),
-			error => dispatch(addVisitFailure(error))
 		);
 };
 
@@ -220,24 +212,43 @@ export const fetchSpecialties = () => (dispatch, getState) => {
 		);
 };
 
+// VISITS
+
+export const fetchVisitsByPatientId = (patientId) => (dispatch) => {
+	dispatch(fetchVisitsRequest());
+
+	return VisitsApi.fetchVisitsByPatient(patientId)
+		.then(handleApiResponse)
+		.then(
+			response => dispatch(fetchVisitsSuccess(normalize(response, schema.visitsListSchema))),
+			error => dispatch(fetchVisitsFailure(error))
+		);
+};
+
+export const addVisit = (visit, patientId) => (dispatch) => {
+	dispatch(addVisitRequest());
+
+	return VisitsApi.addVisit(visit, patientId)
+		.then(handleApiResponse)
+		.then(
+			response => dispatch(addVisitSuccess(normalize([ response ], schema.visitsListSchema))),
+			error => dispatch(addVisitFailure(error))
+		);
+};
+
+// LOGIN
 
 export const login = (user) => (dispatch) => {
 	dispatch(loginRequest());
 
 	return LoginApi.login(user)
-		.then(response => {
-			if (response.ok) {
-				return response.json();
-			}
-
-			throw new Error('Incorrect username or password');
-		})
+		.then(handleApiResponse)
 		.then(
 			response => {
 				localStorage.setItem('token', response.token);
 				dispatch(loginSuccess(response));
 			}, 
-			error => dispatch(loginFailure({ message: error.message }))
+			error => dispatch(loginFailure(error))
 		);
 };
 
