ed870f5a3e767216a1a9a01d0b0ef294141f4647
ljurak <lucas.jurak@gmail.com>
2019-12-30 11:20:05 +0100

    added strategy for generating entity ids

--- model/src/main/java/com/healthcare/model/entities/Visit.java
+++ model/src/main/java/com/healthcare/model/entities/Visit.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -22,7 +23,7 @@ import org.hibernate.annotations.UpdateTimestamp;
 public class Visit {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@ManyToOne
