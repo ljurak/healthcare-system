2b35fcb88a4127db3833071870cd41c1c348a6aa
ljurak <lucas.jurak@gmail.com>
2020-01-01 17:08:09 +0100

    added listener to browser history

--- front/src/components/Root.js
+++ front/src/components/Root.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import App from './App';
 
-const Root = ({ store }) => (
+const Root = ({ store, history }) => (
 	<Provider store={store}>
-		<Router>
+		<Router history={history}>
 			<App />
 		</Router>
 	</Provider>
--- front/src/index.js
+++ front/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createBrowserHistory } from 'history';
 import './index.css';
 
+import { clearAlert } from './actions';
 import Root from './components/Root';
 import configureStore from './store/configureStore';
 
 const store = configureStore();
 
+const history = createBrowserHistory();
+history.listen(location => store.dispatch(clearAlert()));
+
 ReactDOM.render(
-	<Root store={store} />, 
+	<Root store={store} history={history} />, 
 	document.getElementById('root')
 );
