a0cd6560f317591eabd4ca9283c69b4ee4e52f52
ljurak <lucas.jurak@gmail.com>
2020-02-06 21:09:28 +0100

    modified jwt tokens handling in authentication filter

--- rest/src/main/java/com/healthcare/rest/security/JwtAuthenticationFilter.java
+++ rest/src/main/java/com/healthcare/rest/security/JwtAuthenticationFilter.java
@@ -18,40 +18,69 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.healthcare.rest.exception.ApiError;
+import com.healthcare.service.utils.JwtUtils;
 
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {	
 	
+	private static final String AUTHORIZATION_HEADER = "Authorization";
+	private static final String TOKEN_PREFIX = "Bearer ";
+	
 	private ObjectMapper objectMapper;
+	
 	private AuthenticationManager authenticationManager;
+	
 	private JwtUtils jwtUtils;
 	
-	public JwtAuthenticationFilter(AuthenticationManager authenticationManager, JwtUtils jwtUtils, ObjectMapper objectMapper) {
+	public JwtAuthenticationFilter(AuthenticationManager authenticationManager, 
+			JwtUtils jwtUtils, 
+			ObjectMapper objectMapper) {
 		super(authenticationManager);
 		this.authenticationManager = authenticationManager;
 		this.jwtUtils = jwtUtils;
 		this.objectMapper = objectMapper;
 	}
 	
+	@Override
 	public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		UserDetails principal;
-		try {
-			principal = jwtUtils.parseToken(request);
-		} catch (JWTVerificationException ex) {
-			ApiError apiError = new ApiError(HttpStatus.UNAUTHORIZED, "Your token is invalid. Please log in again.");
-			response.setContentType("application/json;charset=UTF-8");
-			response.getWriter().write(objectMapper.writeValueAsString(apiError));
+		String token = extractToken(request);
+		if (token == null) {
 			chain.doFilter(request, response);
 			return;
 		}
 		
-		if (principal == null) {
-			chain.doFilter(request, response);
+		UserDetails principal;
+		try {
+			principal = jwtUtils.parseToken(token);
+		} catch (JWTVerificationException ex) {
+			createInvalidTokenResponse(request, response, chain);
 			return;
 		}
+		
+		if (principal == null) {
+			createInvalidTokenResponse(request, response, chain);
+			return;
+		}
+		
 		Authentication authentication = 
-				new UsernamePasswordAuthenticationToken(principal.getUsername(), null, principal.getAuthorities());
+				new UsernamePasswordAuthenticationToken(principal, null, principal.getAuthorities());
 		SecurityContextHolder.getContext().setAuthentication(authentication);
 		chain.doFilter(request, response);		
-	}	
+	}
+	
+	public String extractToken(HttpServletRequest request) {
+		String authHeader = request.getHeader(AUTHORIZATION_HEADER);
+		if (authHeader != null && authHeader.startsWith(TOKEN_PREFIX)) {
+			return authHeader.replace(TOKEN_PREFIX, "");
+		}
+		return null;
+	}
+	
+	public void createInvalidTokenResponse(HttpServletRequest request, HttpServletResponse response, FilterChain chain) 
+			throws IOException, ServletException {
+		ApiError apiError = new ApiError(HttpStatus.UNAUTHORIZED, "Your token is invalid. Please log in again.");
+		response.setContentType("application/json;charset=UTF-8");
+		response.getWriter().write(objectMapper.writeValueAsString(apiError));
+		chain.doFilter(request, response);
+	}
 }
